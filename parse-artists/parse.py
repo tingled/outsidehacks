6205de254f395cae2e4cc12ca257c5e5465985d9
Ubuntu <ubuntu@ip-10-252-96-98.us-west-2.compute.internal>
2013-08-04 04:09:09 +0000

    ok, durations this time around

--- parse-artists/parse.py
+++ parse-artists/parse.py
@@ -2,6 +2,7 @@ import requests
 import pdb
 import re
 import json
+import codecs
 
 def parse_artists():
   url = 'http://lineup.sfoutsidelands.com/?sort=alpha'
@@ -27,7 +28,7 @@ def parse_artist_page(url):
   return (name,set_time)
 
 def old_get_artist_info():
-"""whoops! forgot to get duration of set... now for a different approach"""
+  """whoops! forgot to get duration of set... now for a different approach"""
   artist_urls = parse_artists()
   artist_info = []
   for artist_url in artist_urls:
@@ -36,3 +37,28 @@ def old_get_artist_info():
       artist_info.append(info)
 
   json.dump(artist_info,open('set_data.json','w'))
+
+def parse_day(day):
+  sets = []
+  stages = {'1':'Lands End','2':'Sutro','3':'Twin Peaks','4':'Panhandle','5':'The Dome','6':'The Barbary'}
+
+  url = 'http://lineup.sfoutsidelands.com/events/2013/08/' + day + '/'
+  page = requests.get(url)
+  search_str = 'href="/band/.*?">(.*?)</a>.*?>(\d.*? - .*?)</span></div>'
+  for line in page.text.split('\n'):
+    if line.find('ds-stage') >=0:
+      stage = stages[re.findall('ds-stage(\d)',line)[0]]
+    match=re.findall(search_str,line)
+    if match:
+      match=match[0]
+      name = match[0]
+      (start,end) = match[1].split(' - ')
+      sets.append((name,str(int(day)),stage,start,end))
+  return sets
+
+days = ['09','10','11']
+sets = []
+for day in days:
+  sets+=parse_day(day)
+
+json.dump(sets,open('set_data.json','w'))
